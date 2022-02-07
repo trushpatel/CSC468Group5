bb9fee69937c9b0826feafe30216fd64cf2c6ed7
KevinCodd <61719515+KevinCodd@users.noreply.github.com>
2022-02-07 17:59:48 -0500

    Update setup_db.sh

--- setup_db.sh
+++ setup_db.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
 #https://dev.mysql.com/doc/mysql-apt-repo-quick-guide/en/#repo-qp-apt-install-from-source
-sudo apt-get build-dep mysql-server
-apt-get source -b mysql-server
+# sudo apt-get build-dep mysql-server
+# apt-get source -b mysql-server
 #WIP
