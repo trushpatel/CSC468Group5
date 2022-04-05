24b064045cc527ef6a18d5987e64cbec8cd909e4
KevinCodd <61719515+KevinCodd@users.noreply.github.com>
2022-04-05 00:43:41 -0400

    Update webui.js

--- webui/webui.js
+++ webui/webui.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.redirect('/index.html');
+  //res.redirect('/index.html');
+  res.send('Welcome to Web Chess!');
 });
 
 app.use(express.static('files'));
