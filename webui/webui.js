d103e52cf2334c029ae20c28804c248b1df329d4
KevinCodd <61719515+KevinCodd@users.noreply.github.com>
2022-04-04 23:51:12 -0400

    modify webui, add index.html

--- webui/webui.js
+++ webui/webui.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.redirect('/index.html');
 });
+
+app.use(express.static('files'));
 
 const server = app.listen(80, () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
+
