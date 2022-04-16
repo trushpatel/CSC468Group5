025acbfc3da705c7d1c96726812bba016266dbe2
trushpatel <68613515+trushpatel@users.noreply.github.com>
2022-04-16 13:55:29 -0400

    Update webui.js

--- webui/webui.js
+++ webui/webui.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  //res.redirect('/index.html');
-  res.send('Welcome to Web Chess!');
+  res.redirect('/index.html');
+  //res.send('Welcome to Web Chess!');
 });
 
 app.use(express.static('files'));
