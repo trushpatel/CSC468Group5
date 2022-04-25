31694a94a6e7046c1d98b0349fc3dc6a63312ccd
Patrick Lawrence <patrickjameslawrence@icloud.com>
2022-04-25 17:33:22 -0400

    Figured out how to run stockfish container in background and included some helpful info in the AI Dockerfile

--- webui/webui.js
+++ webui/webui.js
@@ -1,14 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.redirect('/index.html');
+app.get("/", (req, res) => {
+  res.redirect("/index.html");
   //res.send('Welcome to Web Chess!');
 });
 
-app.use(express.static('files'));
+app.use(express.static("files"));
 
 const server = app.listen(80, () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
-
