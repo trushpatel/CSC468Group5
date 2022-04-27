04fffaf92fbe58f8c2f2469f47269b39e538765f
Patrick Lawrence <patrickjameslawrence@icloud.com>
2022-04-27 17:28:32 -0400

    Added a stockfish api so we can talk to the stockfish terminal

--- ai/ai.py
+++ ai/ai.py
@@ -1,14 +1,27 @@
-import psycopg2
+# import psycopg2
 
-conn = psycopg2.connect( # need to fill this in with actual credentials
-    host="localhost",
-    database="suppliers",
-    user="postgres",
-    password="Abcd1234"
-)
+# conn = psycopg2.connect( # need to fill this in with actual credentials
+#     host="localhost",
+#     database="suppliers",
+#     user="postgres",
+#     password="Abcd1234"
+# )
 
-cur = conn.cursor()
-cur.execute("SELECT version()")
+# cur = conn.cursor()
+# cur.execute("SELECT version()")
 
-version = cur.fetchone()
-print(version)
+# version = cur.fetchone()
+# print(version)
+
+import http
+
+conn = http.client.HTTPSConnection("localhost", 8443)
+conn.request('GET', '/')
+resp = conn.getresponse()
+print(resp.read())
+
+# from stockfish import Stockfish
+# stockfish = Stockfish("/stockfish/stockfish", depth=12)
+# stockfish.set_position(["e2e4", "e7e6"])
+# stockfish.make_moves_from_current_position(["g4d7", "a8b8", "f1d1"])
+
