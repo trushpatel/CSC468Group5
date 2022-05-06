be1928bfd38960b4b0a140070c844f4589bbcc02
trushpatel <68613515+trushpatel@users.noreply.github.com>
2022-05-06 14:36:37 -0400

    Update database.sh

--- webui2/files/database.sh
+++ webui2/files/database.sh
@@ -1,13 +1,5 @@
 !#/bin/sh
 
-database = "kubechess"
-
-psql -U postgres -p postgres
-psql -d $database -c "CREATE EXTENSION IF NOT EXISTS "uuid-ossp";"
-psql -d $database -c "CREATE TABLE users (
-  user_id uuid PRIMARY KEY DEFAULT
-  uuid_generate_v4(),
-  user_name VARCHAR(255) NOT NULL,
-  user_email VARCHAR(255) NOT NULL,
-  user_password VARCHAR(255) NOT NULL
-);"
+apt-get update
+apt-get install sudo
+apt install postgresql -y
