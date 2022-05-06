481d9be74b0fb94f60879e04d60474c951c4a74d
trushpatel <68613515+trushpatel@users.noreply.github.com>
2022-05-06 16:05:41 -0400

    Add files via upload

---
+++ backend/files/database.sh
@@ -0,0 +1,20 @@
+!#/bin/sh
+
+apt-get update
+apt-get install sudo
+apt install postgresql -y
+sudo service postgresql restart
+sudo -u postgres psql << EOF
+CREATE DATABASE kubechess;
+\c kubechess;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE users (
+  user_id uuid PRIMARY KEY DEFAULT
+  uuid_generate_v4(),
+  user_name VARCHAR(255) NOT NULL,
+  user_email VARCHAR(255) NOT NULL,
+  user_password VARCHAR(255) NOT NULL
+);
+INSERT INTO users (user_name, user_email, user_password)
+VALUES('123','123@gmail.com','123');
+EOF
