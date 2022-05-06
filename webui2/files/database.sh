f58e85310c2f35e7e93c7fc68680d4ed4c0df42e
trushpatel <68613515+trushpatel@users.noreply.github.com>
2022-05-06 15:26:50 -0400

    Update database.sh

--- webui2/files/database.sh
+++ webui2/files/database.sh
@@ -15,6 +15,4 @@ CREATE TABLE users (
   user_email VARCHAR(255) NOT NULL,
   user_password VARCHAR(255) NOT NULL
 );
-INSERT INTO users(user_name, user_email, user_password)
-VALUES (123, 123@gmail.com, 123);
 EOF
