b38ade6196dfd2083d69d99654df0ad497dbd84d
trushpatel <68613515+trushpatel@users.noreply.github.com>
2022-05-06 14:42:37 -0400

    Update database.sh

--- webui2/files/database.sh
+++ webui2/files/database.sh
@@ -8,3 +8,11 @@ sudo -u postgres psql
 
 CREATE DATABASE kubechess;
 \c kubechess;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE users (
+  user_id uuid PRIMARY KEY DEFAULT
+  uuid_generate_v4(),
+  user_name VARCHAR(255) NOT NULL,
+  user_email VARCHAR(255) NOT NULL,
+  user_password VARCHAR(255) NOT NULL
+);
