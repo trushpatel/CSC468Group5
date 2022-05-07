b6de25af17079d1943cf35f01125489818b80782
KevinCodd <61719515+KevinCodd@users.noreply.github.com>
2022-05-06 22:56:55 -0400

    update db host

--- backend/files/server/db.js
+++ backend/files/server/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool
 const pool = new Pool({
     user: "postgres",
     password: "postgres",
-    host: "localhost",
+    host: "155.98.37.68",
     port: 5432,
     database:"kubechess"
 })
